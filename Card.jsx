bd1ceb895d0d3c593dfa7e80c36ffb7fee502697
Agus3102 <91124695+Agus3102@users.noreply.github.com>
2022-03-28 20:28:30 +0700

    Add files via upload

---
+++ Card.jsx
@@ -0,0 +1,105 @@
+const ShowTabel = ({ daftarNama }) => {
+  return (
+    <div>
+      <div className="row">
+        <div className="col-lg-3 col-md-4 col-sm-6 mb-3">
+          <div className="card">
+            <img src="img/user.png" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Abdul</h5>
+              <p className="card-text">Alamat : Bulusan</p>
+              <a href="#" className="btn btn-primary">
+                Kunjungi
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 col-md-4 col-sm-6 mb-3">
+          <div className="card">
+            <img src="img/user.png" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Monaji</h5>
+              <p className="card-text">Alamat : Songgon</p>
+              <a href="#" className="btn btn-primary">
+                Kunjungi
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 col-md-4 col-sm-6 mb-3">
+          <div className="card">
+            <img src="img/user.png" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">poniman</h5>
+              <p className="card-text">Alamat : Wongsorejo</p>
+              <a href="#" className="btn btn-primary">
+                Kunjungi
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 col-md-4 col-sm-6 mb-3">
+          <div className="card">
+            <img src="img/user.png" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Hendri</h5>
+              <p className="card-text">Alamat : Ketapang</p>
+              <a href="#" className="btn btn-primary">
+                Kunjungi
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 col-md-4 col-sm-6 mb-3">
+          <div className="card">
+            <img src="img/user.png" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">ANSORI</h5>
+              <p className="card-text">Alamat : Surabaya</p>
+              <a href="#" className="btn btn-primary">
+                Kunjungi
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 col-md-4 col-sm-6 mb-3">
+          <div className="card">
+            <img src="img/user.png" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Suminto</h5>
+              <p className="card-text">Alamat : Pasuruan</p>
+              <a href="#" className="btn btn-primary">
+                Kunjungi
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 col-md-4 col-sm-6 mb-3">
+          <div className="card">
+            <img src="img/user.png" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Lasmono</h5>
+              <p className="card-text">Alamat : Pesucen</p>
+              <a href="#" className="btn btn-primary">
+                Kunjungi
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 col-md-4 col-sm-6 mb-3">
+          <div className="card">
+            <img src="img/user.png" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Nyoman</h5>
+              <p className="card-text">Alamat : Singaraja</p>
+              <a href="#" className="btn btn-primary">
+                Kunjungi
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default ShowTabel;
