e595f7cceb48511e044de0bcbb36a22592445700
Agus3102 <91124695+Agus3102@users.noreply.github.com>
2022-03-29 10:11:13 +0700

    Update Card.jsx

--- Card.jsx
+++ Card.jsx
@@ -6,7 +6,7 @@ const ShowTabel = ({ daftarNama }) => {
           <div className="card">
             <img src="img/user.png" className="card-img-top" alt="..." />
             <div className="card-body">
-              <h5 className="card-title">Abdul</h5>
+              <h5 className="card-title">Tomi</h5>
               <p className="card-text">Alamat : Bulusan</p>
               <a href="#" className="btn btn-primary">
                 Kunjungi
@@ -18,31 +18,7 @@ const ShowTabel = ({ daftarNama }) => {
           <div className="card">
             <img src="img/user.png" className="card-img-top" alt="..." />
             <div className="card-body">
-              <h5 className="card-title">Monaji</h5>
-              <p className="card-text">Alamat : Songgon</p>
-              <a href="#" className="btn btn-primary">
-                Kunjungi
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-3 col-md-4 col-sm-6 mb-3">
-          <div className="card">
-            <img src="img/user.png" className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">poniman</h5>
-              <p className="card-text">Alamat : Wongsorejo</p>
-              <a href="#" className="btn btn-primary">
-                Kunjungi
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-3 col-md-4 col-sm-6 mb-3">
-          <div className="card">
-            <img src="img/user.png" className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Hendri</h5>
+              <h5 className="card-title">Doni</h5>
               <p className="card-text">Alamat : Ketapang</p>
               <a href="#" className="btn btn-primary">
                 Kunjungi
@@ -54,7 +30,31 @@ const ShowTabel = ({ daftarNama }) => {
           <div className="card">
             <img src="img/user.png" className="card-img-top" alt="..." />
             <div className="card-body">
-              <h5 className="card-title">ANSORI</h5>
+              <h5 className="card-title">Raden</h5>
+              <p className="card-text">Alamat : Bali</p>
+              <a href="#" className="btn btn-primary">
+                Kunjungi
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 col-md-4 col-sm-6 mb-3">
+          <div className="card">
+            <img src="img/user.png" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Hendra</h5>
+              <p className="card-text">Alamat : Ketapang</p>
+              <a href="#" className="btn btn-primary">
+                Kunjungi
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 col-md-4 col-sm-6 mb-3">
+          <div className="card">
+            <img src="img/user.png" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Joyo</h5>
               <p className="card-text">Alamat : Surabaya</p>
               <a href="#" className="btn btn-primary">
                 Kunjungi
@@ -78,8 +78,8 @@ const ShowTabel = ({ daftarNama }) => {
           <div className="card">
             <img src="img/user.png" className="card-img-top" alt="..." />
             <div className="card-body">
-              <h5 className="card-title">Lasmono</h5>
-              <p className="card-text">Alamat : Pesucen</p>
+              <h5 className="card-title">Tari</h5>
+              <p className="card-text">Alamat : Genteng</p>
               <a href="#" className="btn btn-primary">
                 Kunjungi
               </a>
